1532967d4594395e59853f335055064a78fc1280
David Bassey <davidosky1@gmail.com>
2021-01-07 10:48:56 +0100

    adds comment model, validations and necessary assiociations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :comments, through: :posts, dependent: :destroy
+
   validates :username,
             presence: true,
             length: { minimum: 6, maximum: 20 },
